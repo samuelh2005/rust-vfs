f0ee0908360f122cabddf4ce1965330249a3c723
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2026-04-12 16:17:13 +0100

    refactor: remove unused modules and types from the VFS implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,3 @@
 #![no_std]
 
 extern crate alloc;
-
-pub mod core;
-pub mod traits;
-pub mod types;
-pub mod errors;
-pub mod memfs;
-pub mod fd;
-
-pub use core::{init, mount, open, read, write, close, stat, fstat, lstat};
-pub use traits::FileSystem;
-pub use types::{Node, NodeKind, Stat};
-pub use errors::{Error, Result};
